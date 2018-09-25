8358fa0e4a4362b275212fb91ad37d81cba2d37f
Daeun <daeuni>
2018-09-25 16:57:51 +0900

    Get location permissions and import my location

--- app/src/main/java/com/dgu/cecd/soycrab/MapsActivity.java
+++ app/src/main/java/com/dgu/cecd/soycrab/MapsActivity.java
@@ -1,18 +1,40 @@
 package com.dgu.cecd.soycrab;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.gun0912.tedpermission.PermissionListener;
+
+import java.security.acl.Permission;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private boolean isGPSEnabled, isNetworkEnabled;
+    private MyPermissionListner permissionListner;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private double myLatitude, myLongtitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +46,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    private class MyPermissionListner implements PermissionListener {
+        private LocationManager locationManager = (LocationManager) MapsActivity.this.getSystemService(Context.LOCATION_SERVICE);
+
+        @Override
+        public void onPermissionGranted() {
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (!isNetworkEnabled || !isGPSEnabled) {
+                Log.v("dek", "permission denied");
+            } else {
+                Log.v("dek", "permission success");
+                getCurrentLocation();
+            }
+        }
+
+        @Override
+        public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+            Toast.makeText(MapsActivity.this, "permission needed : " + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getCurrentLocation() {
+        OnCompleteListener<Location> completeListener = new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful() && task.getResult() != null) {
+                    Location currentlocation = task.getResult();
+                    myLatitude = currentlocation.getLatitude();
+                    myLongtitude = currentlocation.getLongitude();
+                    LatLng myLocationLatLng = new LatLng(myLatitude, myLongtitude);
+                    mapSync();
+                } else {
+                }
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(MapsActivity.this, completeListener);
+    }
+
+    private void mapSync() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapsActivity.this);
+    }
 
     /**
      * Manipulates the map once available.
@@ -37,7 +112,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
