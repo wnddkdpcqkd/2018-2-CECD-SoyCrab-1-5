c85f2a4ac911abbe5001826351ef38e59078ac5e
Daeun <daeuni>
2018-09-25 16:01:24 +0900

    Add Individual Activity Files

---
+++ app/src/main/java/com/dgu/cecd/soycrab/CarxinActivity.java
@@ -0,0 +1,13 @@
+package com.dgu.cecd.soycrab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarxinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carxin);
+    }
+}
