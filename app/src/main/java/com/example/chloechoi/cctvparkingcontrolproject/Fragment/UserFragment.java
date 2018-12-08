99a226ff682cefa6376a6e2ce33709125a9f6271
cchloe2311 <cchloe2311@daum.net>
2018-12-08 23:19:26 +0900

    fixed its error

--- app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/UserFragment.java
+++ app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/UserFragment.java
@@ -1,10 +1,13 @@
 package com.example.chloechoi.cctvparkingcontrolproject.Fragment;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,14 +31,12 @@ public class UserFragment extends Fragment{
 
         Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.user_backgroundpic);
 
-        Bitmap resized = Bitmap.createScaledBitmap(image, 450, 200, true);
+        Point deviceSize = getScreenSize(getActivity());
+
+        Bitmap resized = Bitmap.createScaledBitmap(image, deviceSize.x, deviceSize.y/2, true);
         bckPic.setImageBitmap(resized);
         bckPic.setScaleType(ImageView.ScaleType.CENTER_INSIDE); // 레이아웃 크기에 이미지를 맞춘다
         bckPic.setPadding(3, 3, 3, 3);
-//            iv.setOnClickListener(new OnClickListener(){
-//                public void onClick(View arg0) {
-//                    finish();
-//                }
 
         return rootView;
     }
@@ -45,5 +46,12 @@ public class UserFragment extends Fragment{
         * 1. change user_app_edit img
         * 2. show navi apps's icon
         * 3. set img onclick listener*/
+    }
+
+    public Point getScreenSize(Activity activity) {
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return  size;
     }
 }
