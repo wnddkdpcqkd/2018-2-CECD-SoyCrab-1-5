aeed67270b0e71c0dc2411f5211f2a1a951a3264
cchloe2311 <cchloe2311@daum.net>
2018-12-15 15:33:35 +0900

    solved  HistoryViewHolder extends problem

--- app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/HistoryAdapter.java
+++ app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/HistoryAdapter.java
@@ -16,31 +16,23 @@ import java.util.ArrayList;
  * Created by chloechoi on 12/12/2018.
  */
 
-public class HistoryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryViewHolder> {
     ArrayList<HistoryItem> mItem;
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public HistoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_info_row,parent,false);
         return new HistoryViewHolder(view);
     }
 
     @Override
     /*TODO onBindViewHolder parameterㄱㅏ 안바뀜;;;;*/
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(HistoryViewHolder holder, int position) {
         //holder.tvSimpleInfo.setText(mItem.get(position).simpleInfo);
     }
 
     @Override
     public int getItemCount() {
         return mItem.size();
-    }
-
-    class HistoryViewHolder extends RecyclerView.ViewHolder{
-        private TextView tvSimpleInfo;
-        public HistoryViewHolder(View itemView) {
-            super(itemView);
-            tvSimpleInfo = (TextView) itemView.findViewById(R.id.history_simple_address);
-        }
     }
 }
---
+++ app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/HistoryViewHolder.java
@@ -0,0 +1,19 @@
+package com.example.chloechoi.cctvparkingcontrolproject.Fragment;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.chloechoi.cctvparkingcontrolproject.R;
+
+/**
+ * Created by chloechoi on 15/12/2018.
+ */
+
+public class HistoryViewHolder extends RecyclerView.ViewHolder{
+    private TextView tvSimpleInfo;
+    public HistoryViewHolder(View itemView) {
+        super(itemView);
+        tvSimpleInfo = (TextView) itemView.findViewById(R.id.history_simple_address);
+    }
+}
