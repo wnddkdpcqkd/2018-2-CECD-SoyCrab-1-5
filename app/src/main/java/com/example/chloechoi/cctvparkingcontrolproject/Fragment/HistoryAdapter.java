1e84f0e3e364abd3dc2babee1b9e37c020b4ecaa
cchloe2311 <cchloe2311@daum.net>
2018-12-15 15:28:42 +0900

    filled HistoryAdapter

--- app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/HistoryAdapter.java
+++ app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/HistoryAdapter.java
@@ -1,6 +1,7 @@
 package com.example.chloechoi.cctvparkingcontrolproject.Fragment;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -9,33 +10,37 @@ import com.example.chloechoi.cctvparkingcontrolproject.R;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 /**
  * Created by chloechoi on 12/12/2018.
  */
 
 public class HistoryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-
-    public static class HistoryViewHolder extends RecyclerView.ViewHolder {
-        TextView tvSimpleInfo;
-
-        HistoryViewHolder(View view){
-            super(view);
-            tvSimpleInfo = view.findViewById(R.id.history_simple_address);
-        }
-    }
+    ArrayList<HistoryItem> mItem;
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_info_row,parent,false);
+        return new HistoryViewHolder(view);
     }
 
     @Override
+    /*TODO onBindViewHolder parameterㄱㅏ 안바뀜;;;;*/
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        //holder.tvSimpleInfo.setText(mItem.get(position).simpleInfo);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mItem.size();
+    }
+
+    class HistoryViewHolder extends RecyclerView.ViewHolder{
+        private TextView tvSimpleInfo;
+        public HistoryViewHolder(View itemView) {
+            super(itemView);
+            tvSimpleInfo = (TextView) itemView.findViewById(R.id.history_simple_address);
+        }
     }
 }
--- app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/HistoryItem.java
+++ app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/HistoryItem.java
@@ -4,10 +4,10 @@ package com.example.chloechoi.cctvparkingcontrolproject.Fragment;
  * Created by chloechoi on 12/12/2018.
  */
 
-public class HistoryInfo {
+public class HistoryItem {
     public String simpleInfo;
 
-    public HistoryInfo(String simpleInfo){
+    public HistoryItem(String simpleInfo){
         this.simpleInfo = simpleInfo;
     }
 }
