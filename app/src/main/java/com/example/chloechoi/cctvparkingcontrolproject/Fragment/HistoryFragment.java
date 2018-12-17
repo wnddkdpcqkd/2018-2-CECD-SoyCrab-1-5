fedc157e4d1c7a35833c6c4e392088f7cf16bd63
cchloe2311 <cchloe2311@daum.net>
2018-12-17 17:15:37 +0900

    added calendarView on fragment_history.xml & binded materialCalendarView on HistoryFragment.java

--- app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/HistoryFragment.java
+++ app/src/main/java/com/example/chloechoi/cctvparkingcontrolproject/Fragment/HistoryFragment.java
@@ -2,6 +2,7 @@ package com.example.chloechoi.cctvparkingcontrolproject.Fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,8 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.chloechoi.cctvparkingcontrolproject.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * Created by chloechoi on 28/11/2018.
@@ -23,11 +29,32 @@ public class HistoryFragment extends Fragment {
     private HistoryAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private ArrayList<HistoryItem> mHistoryData;
+    private MaterialCalendarView materialCalendarView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_history,container, false);
+
+        materialCalendarView = (MaterialCalendarView) view.findViewById(R.id.history_calendarView);
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(2017, 0, 1))
+                .setMaximumDate(CalendarDay.from(2030, 11, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();//View);
+
+        materialCalendarView.setOnDateChangedListener
+                (new OnDateSelectedListener(){
+                    @Override
+                    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                        /*TODO send request to server and get the datas*/
+                        return;
+                    }
+                }
+        );
+        /*TODO reference
+        * http://dpdpwl.tistory.com/3#recentComments */
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.history_recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -38,8 +65,6 @@ public class HistoryFragment extends Fragment {
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        /*TODO
-        * 서버에서 데이터받아오는 부분*/
 
         return view;
     }
